bd7e524c692a0c449ec6c31d44614857daffd362
Luis Gordillo C <lgordillo@cecropiasolutions.com>
2020-10-18 19:05:58 -0500

    Sidebar and footer components.

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,20 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { UserOrdersTableComponent } from './user-orders-table/user-orders-table.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    UserOrdersTableComponent
+    UserOrdersTableComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     NavbarComponent,
-    UserOrdersTableComponent
+    UserOrdersTableComponent,
+    SidebarComponent,
+    FooterComponent
   ]
 })
 export class ComponentsModule { }
